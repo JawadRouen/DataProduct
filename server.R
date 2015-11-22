16b04095edf54076151420f700bb59c3c78624a0
Jawad <jawad.alaoui@gmail.com>
2015-11-22 23:06:16 +0100

    new version

--- server.R
+++ server.R
@@ -4,16 +4,14 @@ library(quantmod)
 library(tseries)
 library(ggplot2)
 
-#quantstrat
 
-#^FCHI cac code
-#FP.PA Total
+#^FCHI cac 40 code
 
 getSymbols("^FCHI")
 
 shinyServer(
   function(input, output) {
-
+# plot the stock chart using quandmod package
     output$plotStock <- renderPlot({
       getSymbols(input$stock)
       range <- paste0(input$daterange[1], "::", input$daterange[2])
@@ -21,12 +19,14 @@ shinyServer(
       chartSeries(Stockclose, theme = chartTheme("white"), TA = NULL, name = "Selected Stock")
     })
    
+# plot the CAC 40 chart using quandmod package
     output$plotCAC <- renderPlot({
       range <- paste0(input$daterange[1], "::", input$daterange[2])
       CACclose <<- FCHI$FCHI.Close[range]
       chartSeries(CACclose, theme = chartTheme("white"), TA = NULL, name = "CAC 40")
     })
 
+# computing the retruns and ploting the linear regression
     observeEvent(c(input$stock, input$daterange),{
       
       output$plotReg <- renderPlot({
@@ -38,7 +38,7 @@ shinyServer(
             xlab  = "CAC return", ylab = paste0("Stock return ",input$stock))
       
       })
-
+# summary of the linear regression results
       output$fitSum <- renderPrint({     
        summary(lm(Stockreturn~CACreturn))
       })
--- ui.R
+++ ui.R
@@ -2,19 +2,26 @@ library(shiny)
 shinyUI(pageWithSidebar(
   headerPanel("CAPM Capital Asset Pricing Mode"),
   sidebarPanel(
-    selectInput("stock", "Stock Name:",
+    selectInput("stock", "Select the stock:",
                 c("Total" =  "FP.PA",
                   "Societe Generale" = "GLE.PA",
                   "Renault" = "RNO.PA",
                   "EDF" = "EDF.PA")),
-    dateRangeInput("daterange", "Date range:",
+    dateRangeInput("daterange", "Select the Date range:",
                    min  = "2008-01-01",
                    max    = Sys.Date()-10,
                    start = "2010-01-01",
                    end =   Sys.Date()-10,
                    format = "mm/dd/yy",
                    separator = " - "),
-    submitButton('Submit')
+    submitButton('Submit'),
+    h4("Help:"),
+    h5("The capital asset pricing model (CAPM) assumes that a stock 
+       belonging to a given market, has a return linearly 
+       dependent on the market return"),
+    h5("Select the stock and the period (date range) 
+       of the calculation in order to fit your linear regression."),
+    h5("Plots and summary results are shown in the main panel ")
     
   ),
   mainPanel(
